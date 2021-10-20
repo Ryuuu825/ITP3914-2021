41d38882de579d49111a9ddfa0dd87e651a91f09
Ryuuu825 <85796869+Ryuuu825@users.noreply.github.com>
2021-10-20 21:07:21 +0800

    Update shell script

--- Hey.sh
+++ Hey.sh
@@ -2,7 +2,11 @@
 
 # build date
 DATE=$(date)
-# which os that buil it
+JAVA_VERSION=$(java -version 2>&1 | grep 'version' | awk '{printf $2}' )
+
+#----------------------------------------------
+
+# which os that build it
 case "$OSTYPE" in
   darwin*)  Paltform="OSX" ;; 
   linux*)   Paltform="LINUX" ;;
@@ -10,27 +14,72 @@ case "$OSTYPE" in
   cygwin*)  Paltform="WINDOWS" ;;
 esac
 
+#----------------------------------------------
 
-if [ -f ./build/com/Main.class ]
+  # clean the build before
+if [ -f ./build/com/Main.java ]
 then 
-    # clean the build and recompile it
     rm -r ./build/*
 fi
+#----------------------------------------------
 
 # place the output into ./test/default_testcase.txt
-TESTFILE="./test/default_testcase.txt"
+TESTFILE="./test_log/default_testcase.txt"
 
+# formatting
 printf "\n" >> $TESTFILE
 for ((i=0;i<30;i++)) do
     printf "-" >> $TESTFILE
 done 
 printf "\n" >> $TESTFILE
 
-# put date and platform as header
+# put date and platform and (Result :) as header
 echo "Date" : $DATE >> $TESTFILE
 printf "Platform : %s \n" $Paltform >> $TESTFILE
+printf "JDK version : %s \n" $JAVA_VERSION >> $TESTFILE
+printf "Result:\t" >> $TESTFILE
+
+#----------------------------------------------
+
+printf "[Compiling...]\n" 
+
+compile=$(javac -d "./build"  src/com/Bingo/Player.java src/com/Bingo/Card.java src/com/Bingo/Host.java src/com/Main.java 2> temp.log)
+
+if [[ $? != 0 ]] 
+then 
+  printf "[Compiling Fail]\n" >> $TESTFILE
+  printf "[Compiling Fail]\n"
+
+  error_message=$(cat temp.log)
+  printf  "\noutput :\n\t" >> $TESTFILE
+  echo $error_message >> $TESTFILE 
+
+  #### need to write error message into $TESTFILE
+  printf "[EXIT CODE : -1]" >> $TESTFILE 
+  exit -1
+
+else 
+  printf "[Compiling Success]\n" >> $TESTFILE
+fi
 
 
-printf "Result:\n" >> $TESTFILE
-javac -d "./build" src/com/Main.java src/com/Bingo/Player.java src/com/Bingo/Card.java src/com/Bingo/Host.java 
-java -cp "./build" "com.Main" >> $TESTFILE
+
+
+#----------------------------------------------
+
+
+
+printf "[ Logging ...] -> %s\n"  $TESTFILE
+
+for ((i=0;i<10;i++)) do
+    printf "-" 
+done 
+printf "[Running]"
+for ((i=0;i<10;i++)) do
+    printf "-" 
+done 
+printf "\n"
+
+printf "\nOutput : \n" >> $TESTFILE
+java -cp "./build" "com.Main" 2>> $TESTFILE >> $TESTFILE
+printf "[EXIT CODE : %d] " $? >> $TESTFILE
--- src/com/Main.java
+++ src/com/Main.java
@@ -1,32 +1,7 @@
 package com;
 
-import com.Bingo.*;
-
 public class Main {
     public static void main(String[] args) {
-        String[][] player1Card= { 
-                {"3","2" , "1"},
-                {"5","6" , "7"},
-                {"30","10" , "9"}
-
-        };
-        Card test = new Card (player1Card);
-        Card copyTest = new Card(test);
-               
-        Player testplayer = new Player("Test player" , test );
-        Host game1 = new Host();
-        game1.registerPlayer(testplayer);
-        game1.registerPlayer(new Player("Test player" , test ));
-
-        game1.update(1);
-        game1.update(3);
-        game1.update(2);
-
-        
-        
-        System.out.println(testplayer.bingo());
-
-        
-
+        System.out.println("Test");
     }
 }
